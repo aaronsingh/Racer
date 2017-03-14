809de62c9672e8216665d1e575aedafab8faae04
Singh <singha12@lancs.local>
2017-03-14 15:06:39 +0000

    Second Working Build with Start/Stop Buttons

--- ArcadeGame.java
+++ ArcadeGame.java
@@ -7,42 +7,43 @@ public class ArcadeGame implements ActionListener              //making a class 
 	JFrame window;
 	JPanel BPanel;
 	JPanel RPanel;
-	JPanel HPanel;
+	JPanel SPanel;
+	JLabel scoreL;
 	JPanel mainPanel;
 	JButton startB, stopB;
-	int ScreenHeight = 700;            // Declare the (integer) vairables for the hieght of the screen 
+	int ScreenHeight = 800; // Declare the (integer) vairables for the hieght of the screen 
 	int ScreenWidth = 800;	// Declare the (integer) vairables for the width of the screen 
 	int Score = 0;
 	Racer r;
 	FlowLayout f;
-	JLabel highScore;
 	
 public ArcadeGame()
 {
 	window = new JFrame();
+	mainPanel = new JPanel();
 	BPanel = new JPanel();  //The panels will help me group all the componants together
 	RPanel = new JPanel();
-	HPanel = new JPanel();
-	mainPanel = new JPanel();	
+	SPanel = new JPanel();
+	scoreL = new JLabel ("Score:");
 	startB = new JButton("Start");  // assigning the startB variable name to Start 
 	stopB = new JButton("Stop");   // assigning the StopB variable name to Stop
+
 	r = new Racer();
 	f = new FlowLayout();
-	JLabel highScore = new JLabel("Score:0");
 	
+	SPanel.add(scoreL);
 	BPanel.add(startB);
 	BPanel.add(stopB);
-	HPanel.add(highScore);
 	window.setTitle("Racer");    
 	window.setContentPane(mainPanel);
 	window.setSize(ScreenWidth, ScreenHeight); // Sets the componants of the Jframe
 	RPanel.add(r.getPanel());
+	SPanel.setLayout(f);	
 	RPanel.setLayout(f);
 	BPanel.setLayout(f);
-	HPanel.setLayout(f);
 	mainPanel.add(RPanel);
 	mainPanel.add(BPanel);
-	mainPanel.add(HPanel);
+	mainPanel.add(SPanel);
 	
 	// The buttons are registered to the addActionListener() method so we can click the buttons
 	startB.addActionListener(this); 
@@ -50,25 +51,21 @@ public ArcadeGame()
 	window.setVisible(true);
 	BPanel.setVisible(true);
 	RPanel.setVisible(true);
-	HPanel.setVisible(true);
-	
-	r.start();
-	
-	while(r.isPlaying())                 // the game will be updating throughout
-      r.update();
+	SPanel.setVisible(true);
 }
 
 public void actionPerformed(ActionEvent e)
 	{
-		if(e.getSource() == startB)
+		if(e.getSource()==startB)
 		r.start();
 		
-		if(e.getSource() == stopB)
-			r.stop();
+		if(e.getSource()==stopB)
+		r.stop();
 	}
-      
-public static void main(String args[])  // main operator
+	
+public static void main(String args[])  //main operator
 	{
 	ArcadeGame a = new ArcadeGame();
+	a.play();
 	}
-}   
+} 
